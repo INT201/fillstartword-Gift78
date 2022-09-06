af1770bb2dbcfcf1b0a2935b377240503510f140
Gift78 <wimonsiri.2546@mail.kmutt.ac.th>
2022-09-06 14:16:01 +0700

    done.

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,14 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if(startWord===null || word===null || startWord, word){
+    return undefined
+  }else {
+    if (word.includes(startWord)){
+      return word
+    }else {
+      return startWord + word
+    }
+  }
 }
 module.exports = fillStartWord
